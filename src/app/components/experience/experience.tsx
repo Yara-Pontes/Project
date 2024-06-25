dbf4b53d00670ef8f8890cbd1f6da4ae12446602
Seu Nome <yarapontes024@gmail.com>
2024-06-25 12:55:18 -0300

    Program

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -10,12 +10,10 @@ export function Experience(){
           <SectionTitle text="Experience"/>
           <p>3 years working as a Software Developer</p>
           <div className="experience-time">
-            <Skill image="/charp.png" measure={3} years="2 years"/>
-            <Skill image="/mysql.png" measure={3} years="2 years"/>
-            <Skill image="/node.js.png" measure={3} years="2 years"/>
-            <Skill image="/react.png" measure={3} years="2 years"/>
-            <Skill image="/ts.png" measure={3} years="2 years"/>
-            <Skill image="/js.png" measure={3} years="2 years"/>
+            <Skill image="/charp.png" measure={3} years="3 years"/>
+            <Skill image="/mysql.png" measure={3} years="3 years"/>
+            <Skill image="/ts.png" measure={3} years="3 years"/>
+            <Skill image="/js.png" measure={3} years="3 years"/>
           </div>
         </div>
     )
